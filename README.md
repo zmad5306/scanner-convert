878274a56ec1a1cf7c78386a2fbd5c472d7ba790
zmad5306 <zachary.maddox@gmail.com>
2018-08-23 23:21:43 -0400

    intial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Convert Scanner Auto to Text
+
+Using Bing Text to Speech API
+
+## Local Execution
+
+`npm run start <Bing/Azure text to speech key here> samples/broadcastify01.wav`
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+
+const speechService = require('ms-bing-speech-service');
+
+const args = process.argv.slice(2);
+const subscriptionKey = args[0];
+const file = args[1];
+
+(async function() {
+
+  const options = {
+    language: 'en-US',
+    subscriptionKey: subscriptionKey
+  };
+	
+  const recognizer = new speechService(options);
+  await recognizer.start();
+
+  recognizer.on('recognition', (e: any) => {
+    if (e.RecognitionStatus === 'Success') console.log(e);
+  });
+  
+  recognizer.on('turn.end', async (e: any) => {
+    console.log('recognizer is finished.');
+    
+    await recognizer.stop();
+    console.log('recognizer is stopped.');
+  });
+	
+  await recognizer.sendFile(file);
+  console.log('file sent.');
+
+})();
